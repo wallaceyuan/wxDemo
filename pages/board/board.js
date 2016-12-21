234833b9dfb2f8d3cdde8f349c80dcbb051e9c8b
wallaceyuan <1272864289@qq.com>
2016-12-21 11:34:28 +0800

    search

--- pages/board/board.js
+++ pages/board/board.js
@@ -4,6 +4,11 @@ var API_URL = 'https://api.douban.com/v2/book'
 
 Page({
   data: {
+    page: 1,
+    size: 20,
+    subtitle: '釜山行、夏有乔木仰望天堂',
+    searching: false,
+    search: '',
     books: [],
     loading: true,
     id: util.predict(Math.ceil(Math.random() * 3))
@@ -16,6 +21,10 @@ Page({
     //this.test()
   },
   onPullDownRefresh() {
+    this.setData({
+      searching: false,
+      search:''
+    })
     this.getData(util.predict(Math.ceil(Math.random() * 3)))
   },
   getData(id) {
@@ -30,6 +39,47 @@ Page({
       .catch(function (e) {
         console.error(e)
         that.setData({ books: [], loading: false })
+      })
+  },
+  onBindFocus(){
+    
+  },
+  lower(e) {
+    var that = this
+    console.log('lower')
+    if (!this.data.hasMore) return
+    this.setData({ subtitle: '加载中...', loading: true })
+    douban.find('search', this.data.page++, this.data.size, this.data.search)
+      .then(function (d) {
+        if (d.books.length) {
+          that.setData({ books: that.data.books.concat(d.books), loading: false })
+        } else {
+          that.setData({ hasMore: false, loading: false })
+        }
+      })
+      .catch(function (e) {
+        console.error(e)
+        that.setData({ subtitle: '获取数据异常', loading: false })
+      })
+  },
+  search(e) {
+    this.setData({
+      searching: true
+    })
+    var that = this
+    if (!e.detail.value) return
+    this.setData({ subtitle: '加载中...', hasMore: true, loading: true, search: e.detail.value })
+    douban.find('search', this.data.page++, this.data.size, this.data.search)
+      .then(function (d) {
+        if (d.books.length) {
+          that.setData({ books: d.books, loading: false })
+        } else {
+          that.setData({ hasMore: false, loading: false })
+        }
+      })
+      .catch(function (e) {
+        that.setData({ subtitle: '获取数据异常', books: [], loading: false })
+        console.error(e)
       })
   },
   test() {
